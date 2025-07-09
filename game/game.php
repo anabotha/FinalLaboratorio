b5390baa01438b5b15b06d48a0a3297651cccf1e
Ana Botha <josefinabotha@gmail.com>
2025-07-09 19:37:49 -0300

    css de game

--- game/game.php
+++ game/game.php
@@ -9,19 +9,21 @@
 
 </head>
 <body>
+<h1> Memory Game</h1>
+
      <div id="view-container" class="view-container">
      <div id="cont-jugador1" class="cont-jugador1">
           <h2 id="jugador1"></h2>
-          <div id="intentos" class="intentos">Intentos: <span id="intentos-1"></span></div>
-          <div id="aciertos" class="aciertos">Aciertos:<span id="aciertos-1"></span> </div>
+          <div id="intentos" class="intentos"><p>Intentos: <span id="intentos-1"></span></p></div>
+          <div id="aciertos" class="aciertos"><p>Aciertos:<span id="aciertos-1"></span> </p></div>
      </div>
      <div id="game-container" class="game-container"> </div>
      <div id="cont-jugador2" class="cont-jugador2">
           <h2 id="jugador2"></h2>
-          <div id="intentos" class="intentos">Intentos: <span id="intentos-2"></span></div>
-          <div id="aciertos" class="aciertos">Aciertos:<span id="aciertos-2"></span> </div>
+          <div id="intentos" class="intentos"><p>Intentos: <span id="intentos-2"></span></p></div>
+          <div id="aciertos" class="aciertos"><p>Aciertos:<span id="aciertos-2"></span> </p></div>
      </div>
 </div>
-<div> <p id="info"></p></div>
+<div> <p id="info" class="info"></p></div>
 </body>
 </html>
